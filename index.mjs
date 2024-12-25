a292852ce7c173ffc5e65b452ee0634c8e19163a
Harshith Ashvi <harshith.ashvi@gmail.com>
2024-12-25 14:01:36 +0530

    add inquirer package and prompts for question

--- index.mjs
+++ index.mjs
@@ -1,3 +1,47 @@
 #!/usr/bin/env node
 
-console.log("Hello Figma");
+import { input, select } from "@inquirer/prompts";
+
+const projectName = await input({
+  message: "Project name: ",
+  default: "my-figma-plugin",
+  required: true,
+});
+
+const framework = await select({
+  message: "Select a framework",
+  required: true,
+  default: "vanilla",
+  choices: [
+    {
+      name: "vanilla",
+      value: "vanilla",
+    },
+    {
+      name: "react",
+      value: "react",
+    },
+  ],
+});
+
+const editorType = await select({
+  message: "Select a editor type",
+  required: true,
+  default: "both",
+  choices: [
+    {
+      name: "Figma",
+      value: "figma",
+    },
+    {
+      name: "Figjam",
+      value: "figjam",
+    },
+    {
+      name: "Both",
+      value: "both",
+    },
+  ],
+});
+
+console.log("Hello Figma", projectName, framework, editorType);
